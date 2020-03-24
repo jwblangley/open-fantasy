b3b5b1b608f5f619889e20b14c68da77e4acdd73
James Langley <jwblangley@gmail.com>
2020-03-24 14:49:53 +0000

    Setup material-ui workflow

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="Main">
+      Hello
+    </div>
+  )
+}
+
+export default Main
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      light: '#7d87df',
+      dark: '#0d317d',
+      main: '#4b5aad',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      light: '#ffb66e',
+      dark: '#dd6e00',
+      main: '#ff9b38',
+      contrastText: '#ffffff'
+    }
+  }
+})
